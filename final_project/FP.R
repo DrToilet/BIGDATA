f96fef4da8dd3d3c60d120453292dde8810e81c0
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2017-12-21 16:38:37 +0100

    minor

--- final_project/FP.R
+++ final_project/FP.R
@@ -4,8 +4,9 @@ MY_WD <-("~/Desktop/BIGDATA/final_project")
 PATH <-("/Users/Giacomo/Desktop/BIGDATA/final_project/HR_comma_sep.csv")
 
 # INSTALL ALL DEPENDENCIES
-install.packages(rpart.plot)
-install.packages("e1071")
+#install.packages(rpart.plot)
+#install.packages("e1071")
+#install.packages("ROCR")
 
 # RUN THE CODE
 
@@ -17,29 +18,42 @@ head(myData)
 table(myData$left)
 
 
-#changing categorical data to numerical data
+# changing categorical data to numerical data
 myData$sales<- factor(myData$sales,levels = c("sales","accounting","hr","technical","support","management","IT","product_mng","marketing","RandD"),labels = c(1,2,3,4,5,6,7,8,9,10))
 myData$salary <- factor(myData$salary,levels=c("low","medium","high"),labels =c(0,1,2))
 
 
 library("rpart.plot")
-
+# splitting to train and test set
 set.seed(1)
 train.idx <- sample(1:nrow(myData), size = round(0.8 * nrow(myData)), replace = FALSE)
 train.set <- myData[train.idx,]
 test.set <- myData[-train.idx,]
 
+# creating a decision tree
 dt <- rpart(left ~ ., data =train.set , method = 'class')
+
+# plot tree in 2 different ways
 plot(dt)
 text(dt, pretty = 0)
 rpart.plot(dt)
 
-dtPred <- predict(dt,train.set,type='class')
-table(dtPred,train.set$left)
 
+library('ROCR')
 
+#ROC curve
+dtPred <- predict(dt,newdata=test.set,type="prob")[,2]
+table(dtPred,test.set$left)
+pred2 = prediction(dtPred,test.set$left)
+plot(performance(pred2,"tpr","fpr"))
+abline(0, 1, lty = 2)
 
-#SVM not working
+# AUC
+auc <- unlist(attr(performance(pred2, "auc"), "y.values"))
+print(auc)
+legend("bottomright", sprintf("%.3f",auc), title = "AUC")
+
+# SVM not working
 library("e1071")
 svm.model <- svm(formula = left ~ ., data = train.set, kernel = "radial", cost = 1, probability = TRUE)
 svm.pred <-predict(svm.model, newdata = test.set,probability = TRUE) 
